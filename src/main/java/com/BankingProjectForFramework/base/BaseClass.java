6a0db3968cdb675439c576f447ddf4fabd492382
Dipali-Kitey <dipalikitey2020@gmail.com>
2023-01-25 12:41:54 +0530

    My First Commit

---
+++ src/main/java/com/BankingProjectForFramework/base/BaseClass.java
@@ -0,0 +1,18 @@
+package com.BankingProjectForFramework.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	 
+			public static WebDriver driver;
+			public  void launchWebsite() {
+			 String projectpath = System.getProperty("user.dir");
+			 System.out.println( projectpath);
+			 System.setProperty("webdriver.chrome.driver",projectpath+"\\Driver\\chromedriver.exe");
+			 driver = new ChromeDriver();
+			 driver.get("https://demo.guru99.com/V1/index.php");
+				
+
+			}
+}
---
+++ src/main/java/com/BankingProjectForFramework/pom/LoginpagePom.java
@@ -0,0 +1,38 @@
+package com.BankingProjectForFramework.pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.BankingProjectForFramework.base.BaseClass;
+
+
+public class LoginpagePom extends BaseClass {
+	public LoginpagePom() {
+		PageFactory.initElements(driver, this);
+	  
+	}
+	@FindBy(xpath = "//input[@name='uid']")
+	WebElement userid;
+	@FindBy(xpath ="//input[@name='password']" )
+	WebElement password;
+	@FindBy(xpath ="//input[@name='btnLogin']" )
+	WebElement loginbutton;
+	@FindBy(xpath = "//input[@name='btnReset']")
+	WebElement resetbutton;
+	@FindBy(xpath = "//a[@href='http://demo.guru99.com/'] ")
+	WebElement getcredentiallink;
+	public void clickonloginbutton() {
+		
+		loginbutton.click();
+	}
+	
+	
+	
+		
+	
+	
+}
+	
+
+
